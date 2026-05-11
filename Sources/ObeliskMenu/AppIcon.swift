43337321f3a3e6223ab0a73daaa6ef9f72fe7c80
imeelinew <imeelinew@gmail.com>
2026-05-11 15:37:55 +0800

    添加 Xcode 工程支持

--- Sources/ObeliskMenu/AppIcon.swift
+++ Sources/ObeliskMenu/AppIcon.swift
@@ -2,10 +2,7 @@ import AppKit
 
 enum AppIcon {
     static func menuBarImage() -> NSImage {
-        let symbol = (Bundle.main.resourceURL?.appendingPathComponent("PyramidSymbol.svg"))
-            .flatMap(NSImage.init(contentsOf:))
-            ?? Bundle.module.url(forResource: "PyramidSymbol", withExtension: "svg")
-            .flatMap(NSImage.init(contentsOf:))
+        let symbol = resourceImage(name: "PyramidSymbol", extension: "svg")
             ?? image(size: NSSize(width: 18, height: 18))
 
         let copy = symbol.copy() as? NSImage ?? symbol
@@ -15,10 +12,7 @@ enum AppIcon {
     }
 
     static func image(size: NSSize? = nil) -> NSImage {
-        let base = (Bundle.main.resourceURL?.appendingPathComponent("AppIcon.png"))
-            .flatMap(NSImage.init(contentsOf:))
-            ?? Bundle.module.url(forResource: "AppIcon", withExtension: "png")
-            .flatMap(NSImage.init(contentsOf:))
+        let base = resourceImage(name: "AppIcon", extension: "png")
             ?? NSImage(systemSymbolName: "bookmark.fill", accessibilityDescription: "Obelisk")
             ?? NSImage(size: NSSize(width: 18, height: 18))
 
@@ -28,5 +22,20 @@ enum AppIcon {
         copy.size = size
         copy.isTemplate = false
         return copy
+    }
+
+    private static func resourceImage(name: String, extension pathExtension: String) -> NSImage? {
+        (Bundle.main.resourceURL?.appendingPathComponent("\(name).\(pathExtension)"))
+            .flatMap(NSImage.init(contentsOf:))
+            ?? packageResourceURL(name: name, extension: pathExtension)
+            .flatMap(NSImage.init(contentsOf:))
+    }
+
+    private static func packageResourceURL(name: String, extension pathExtension: String) -> URL? {
+        #if SWIFT_PACKAGE
+        Bundle.module.url(forResource: name, withExtension: pathExtension)
+        #else
+        nil
+        #endif
     }
 }

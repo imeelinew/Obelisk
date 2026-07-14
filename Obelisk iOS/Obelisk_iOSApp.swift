e958241cf0268e1000a12ac5f171d90dbb7debb4
Eli <imeelinew@gmail.com>
2026-07-14 12:00:02 +0800

    Add iOS target scaffold

---
+++ Obelisk iOS/Obelisk_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Obelisk_iOSApp.swift
+//  Obelisk iOS
+//
+//  Created by Eli New on 2026.07.14.
+//
+
+import SwiftUI
+
+@main
+struct Obelisk_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Obelisk iOSTests/Obelisk_iOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  Obelisk_iOSTests.swift
+//  Obelisk iOSTests
+//
+//  Created by Eli New on 2026.07.14.
+//
+
+import Testing
+@testable import Obelisk_iOS
+
+struct Obelisk_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
